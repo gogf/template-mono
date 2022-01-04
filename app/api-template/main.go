4f526fa3ec9e21dd8933d49869c4543d41fff7f7
john <john@johng.cn>
2022-01-04 23:55:41 +0800

    up

--- app/api-template/main.go
+++ app/api-template/main.go
@@ -3,16 +3,10 @@ package main
 import (
 	_ "github.com/gogf/template-mono/app/api-template/internal/packed"
 
-	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gctx"
 	"github.com/gogf/template-mono/app/api-template/internal/cmd"
 )
 
 func main() {
-	var (
-		ctx = gctx.New()
-	)
-	if err := cmd.Main.Run(ctx); err != nil {
-		g.Log().Fatal(ctx, err)
-	}
+	cmd.Main.Run(gctx.New())
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/gogf/template-mono
 go 1.14
 
 require (
-	github.com/gogf/gf/v2 v2.0.0-beta.0.20211223140028-b79e4a3ab7a9
+	github.com/gogf/gf/v2 v2.0.0-rc.0.20220104132444-99455e328b35
 	go.opentelemetry.io/otel v1.2.0 // indirect
 )
